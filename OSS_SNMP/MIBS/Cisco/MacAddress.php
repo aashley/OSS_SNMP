61d40e43a08aa6dd3e3e29ff85bd036636768e71
Adam Ashley <adama@swiftnetworks.com.au>
2013-01-09 10:31:17 +0800

    Add enough to find out which interface a macaddress was last seen on

--- OSS_SNMP/MIBS/Cisco/MacAddress.php
+++ OSS_SNMP/MIBS/Cisco/MacAddress.php
@@ -7,9 +7,21 @@ use OSS_SNMP\MIB;
 class MacAddress extends MIB
 {
     const OID_ADDRESS = '.1.3.6.1.2.1.17.4.3.1.1';
+    const OID_PORTS = '.1.3.6.1.2.1.17.4.3.1.2';
+    const OID_BRIDGE = '.1.3.6.1.2.1.17.1.4.1.2';
 
     public function addresses()
     {
         return $this->getSNMP()->walkMacAddress(self::OID_ADDRESS);
+    }
+
+    public function ports()
+    {
+        return $this->getSNMP()->walkMacAddress(self::OID_PORTS);
+    }
+
+    public function bridgePorts()
+    {
+        return $this->getSNMP()->walk1d(self::OID_BRIDGE);
     }
 }
